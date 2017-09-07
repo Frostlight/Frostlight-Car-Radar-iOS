d6021a4bea1f2fce8a0e2e7024561cccc473b514
Vincent <cloudy.sonata@gmail.com>
2017-09-07 15:12:44 -0400

    Some refactoring and minor changes

---
+++ Frostlight Car Radar/RadarViewController.swift
@@ -0,0 +1,33 @@
+//
+//  RadarViewController.swift
+//  Frostlight Car Radar
+//
+//  Created by Vincent on 9/5/17.
+//  Copyright © 2017 Frostlight. All rights reserved.
+//
+
+import UIKit
+
+class RadarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+}
+
